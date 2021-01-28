01176e307c93581debbd6fda3969bdc820d65ffb
Young-Eun Park <52953945+young-eunpark@users.noreply.github.com>
2021-01-28 23:57:24 +0900

    Update p3.c

--- Jungol/p3.c
+++ Jungol/p3.c
@@ -3,12 +3,11 @@
 /*int main() {
 	int num;
 
-	printf("¼ýÀÚÀÔ·Â : ");
+	printf("숫자입력 : ");
 	scanf_s("%d", &num);
 
 	int i,j;
 	int n = 1;
-	int arr[101][101];
 	for (i = 0; i < num; i++) {
 		for (j = 0; j < num; j++) {
 			printf("%2d", (i + n) + (num * j));
@@ -30,7 +29,7 @@ void makeSquare(int a) {
 int main() {
 	int num;
 
-	printf("¼ýÀÚÀÔ·Â : ");
+	printf("숫자입력 : ");
 	scanf_s("%d", &num);
 
 	makeSquare(num);
