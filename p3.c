704600b6e32593fb1e2a888da2eaaf76edf80337
Young-Eun Park <52953945+young-eunpark@users.noreply.github.com>
2021-01-28 23:56:23 +0900

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*int main() {
+	int num;
+
+	printf("숫자입력 : ");
+	scanf_s("%d", &num);
+
+	int i,j;
+	int n = 1;
+	int arr[101][101];
+	for (i = 0; i < num; i++) {
+		for (j = 0; j < num; j++) {
+			printf("%2d", (i + n) + (num * j));
+		}
+		printf("\n");
+	}
+}*/
+
+void makeSquare(int a) {
+	int i, j;
+	int n = 1;
+	for (i = 0; i < a; i++) {
+		for (j = 0; j < a; j++) {
+			printf("%2d", (i + n) + (a * j));
+		}
+		printf("\n");
+	}
+}
+int main() {
+	int num;
+
+	printf("숫자입력 : ");
+	scanf_s("%d", &num);
+
+	makeSquare(num);
+}
